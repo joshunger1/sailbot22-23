a9c499bdc80c0ea04ecc6bbd0fa2cb36e9fa8c13
Joshua Unger <jhunger@wpi.edu>
2023-01-25 21:04:44 -0500

    geo-coordinate polygon working

--- sailbot_ws/src/sailbot/sailbot/autonomous/waypoint.py
+++ sailbot_ws/src/sailbot/sailbot/autonomous/waypoint.py
@@ -10,6 +10,9 @@ Owen Pfannenstiehl
 Imports
 """
 import numpy as np
+import matplotlib as plt
+from turfpy.measurement import boolean_point_in_polygon
+from geojson import Point, Polygon, Feature
 
 
 
@@ -29,5 +32,41 @@ class Coordinate():
 """
 Main Algorithm
 """
-val = input("Enter your value: ")
-print(val)
+def createPolygonArray(arr):
+    newArr = []
+    for coord in arr:
+        newArr.append((coord.lat, coord.lng))
+    return newArr
+    
+
+
+val = input("How many verticies will the bounding box contain? ")
+boundingBoxCoordinates = []
+
+for x in range(int(val)):
+    newCoordLat = input("Latitude:")
+    newCoordLng = input("Longitude:")
+    waypointNum = x+1
+    waypointFinal = Coordinate(newCoordLat, newCoordLng)
+    boundingBoxCoordinates.append(waypointFinal)
+    print("Created waypoint number " + str(waypointNum) + " with latitude of " + str(newCoordLat) + " and longitude of " + str(newCoordLng) + ".")
+
+tupleCoordinateArr = []
+for coord in boundingBoxCoordinates:
+    coordtuple = tuple([float(coord.lat), float(coord.lng)])
+    tupleCoordinateArr.append(coordtuple)
+
+print(tupleCoordinateArr)
+desiredLat = input("Latitude:")
+desiredLng = input("Longitude:")
+desiredWaypoint = Coordinate(newCoordLat, newCoordLng)
+
+point = Feature(geometry=Point((float(desiredWaypoint.lat), float(desiredWaypoint.lng))))
+
+polygon = Polygon(
+    [
+        tupleCoordinateArr
+    ]
+)
+isWithin = boolean_point_in_polygon(point, polygon)
+print(isWithin)
