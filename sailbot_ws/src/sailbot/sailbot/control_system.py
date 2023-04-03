ae6eda7327590d412b967776245b444bdae26550
Joshua Unger <jhunger@wpi.edu>
2023-04-02 21:56:48 -0400

    alg stuff

--- sailbot_ws/src/sailbot/sailbot/control_system.py
+++ sailbot_ws/src/sailbot/sailbot/control_system.py
@@ -12,14 +12,14 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
 
     def __init__(self):
         super().__init__('control_system')
-        # Create subcription to serial_rc topic
+        # Create subscription to serial_rc topic
         self.serial_rc_subscription = self.create_subscription(
             String,
             'serial_rc',
             self.serial_rc_listener_callback,
             10)
         self.serial_rc_subscription
-        
+
         # Create subscription to airmar_data
         self.airmar_data_subscription = self.create_subscription(
             String,
@@ -27,7 +27,7 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
             self.airmar_data_listener_callback,
             10)
         self.airmar_data_subscription
-        
+
         # Create subscription to tt_telemetry
         self.trim_tab_telemetry_subscription = self.create_subscription(
             Float32,
@@ -35,7 +35,7 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
             self.trim_tab_telemetry_listener_callback,
             10)
         self.trim_tab_telemetry_subscription
-        
+
         # Create publisher to pwm_control
         self.pwm_control_publisher_ = self.create_publisher(String, 'pwm_control', 10)
 
@@ -43,8 +43,8 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
         self.trim_tab_control_publisher_ = self.create_publisher(Int8, 'tt_control', 10)
         self.trim_tab_angle_publisher_ = self.create_publisher(Int16, 'tt_angle', 10)
 
-        # Create publisher to ballast_algorithnm_debug
-        self.ballast_algorithnm_debug_publisher_ = self.create_publisher(String, 'ballast_algorithnm_debug', 10)
+        # Create publisher to ballast_algorithm_debug
+        self.ballast_algorithm_debug_publisher_ = self.create_publisher(String, 'ballast_algorithm_debug', 10)
 
         # Create instance vars for subscribed topics to update
         self.serial_rc = {}
@@ -60,20 +60,19 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
         self.alpha = deque(maxlen=3)
         self.lastRollAngle = deque(maxlen=4)
         # self.p2p_alg = None
-        
 
     def serial_rc_listener_callback(self, msg):
         self.get_logger().info('Received msg: "%s"' % msg.data)
         msg_dict = json.loads(msg.data)
         for i in msg_dict:
             self.serial_rc[i] = msg_dict[i]
-        
+
     def airmar_data_listener_callback(self, msg):
         self.get_logger().info('Received msg: "%s"' % msg.data)
         msg_dict = json.loads(msg.data)
         for i in msg_dict:
             self.airmar_data[i] = msg_dict[i]
-        
+
     def trim_tab_telemetry_listener_callback(self, msg):
         self.get_logger().info('Received msg: "%s"' % msg.data)
 
@@ -84,9 +83,9 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
 
     def update_winds(self, relative_wind):
         # Check we have new wind
-        if len(self.lastWinds) != 0 and relative_wind == self.lastWinds[len(self.lastWinds) -1]:
-            return       
-        # First add wind to running list
+        if len(self.lastWinds) != 0 and relative_wind == self.lastWinds[len(self.lastWinds) - 1]:
+            return
+            # First add wind to running list
         self.lastWinds.append(float(relative_wind))
         if len(self.lastWinds) > 10:
             self.lastWinds.pop(0)
@@ -94,7 +93,7 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
         smooth_angle = self.median(self.lastWinds)
         return smooth_angle
 
-    def find_trim_tab_state(self, relative_wind):           #five states of trim
+    def find_trim_tab_state(self, relative_wind):  # five states of trim
         smooth_angle = self.update_winds(relative_wind)
         msg = Int8()
         if 45.0 <= smooth_angle < 135:
@@ -113,19 +112,18 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
             # In irons, min lift
             msg.data = (4)
 
-        
         self.trim_tab_control_publisher_.publish(msg)
-            
+
     def make_json_string(self, json_msg):
         json_str = json.dumps(json_msg)
         message = String()
         message.data = json_str
         return message
-           
+
     def median(self, lst):
         n = len(lst)
         s = sorted(lst)
-        return (sum(s[n//2-1:n//2+1])/2.0, s[n//2])[n % 2] if n else None
+        return (sum(s[n // 2 - 1:n // 2 + 1]) / 2.0, s[n // 2])[n % 2] if n else None
 
     def ballast_algorithm(self):
         # Check wind angle, then check current tilt of boat, then adjust ballast accordingly
@@ -134,31 +132,31 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
         self.lastRollAngle.append(self.airmar_data["roll"])
         smooth_angle = self.median(self.lastWinds)
         ballast_angle = 0
-        #print("roll:" + self.airmar_data["roll"])
+        # print("roll:" + self.airmar_data["roll"])
         delta = self.airmar_data["roll"] - self.lastRollAngle[-1]
-        
 
-        timeDifference = .5     #hypothetically - see main
-        omega_n = delta/timeDifference
+        timeDifference = .5  # hypothetically - see main
+        omega_n = delta / timeDifference
         self.omega.append(omega_n)
-        alpha_n = self.omega[-1]/timeDifference
+        alpha_n = self.omega[-1] / timeDifference
         self.alpha.append(alpha_n)
-        #-- Logging ----------------
-        self.ballast_algorithnm_debug_publisher_.publish("omega: " + str(omega_n) + " -- " + "alpha / acceleration: " + str(alpha_n) + "\n")
-        #Account for a heavy tilt
-        
-         #-----------
-         # Starboard tack
-        if 0 < smooth_angle <= 180: 
+        # -- Logging ----------------
+        self.ballast_algorithm_debug_publisher_.publish(
+            "omega: " + str(omega_n) + " -- " + "alpha / acceleration: " + str(alpha_n) + "\n")
+        # Account for a heavy tilt
+
+        # -----------
+        # Starboard tack
+        if 0 < smooth_angle <= 180:
             # Go for 20 degrees
-            if float(self.airmar_data["roll"]) > -12:           #change to roll acc.
-                #ballast_angle = 110
+            if float(self.airmar_data["roll"]) > -12:  # change to roll acc.
+                # ballast_angle = 110
                 ballast_angle = omega_n * 2
-            elif float(self.airmar_data["roll"]) < -20:         #change to roll acc.
+            elif float(self.airmar_data["roll"]) < -20:  # change to roll acc.
                 ballast_angle = 80
-                #-----------
+                # -----------
         # Port tack
-        elif 180 < smooth_angle < 360:  
+        elif 180 < smooth_angle < 360:
             if float(self.airmar_data["roll"]) < 12:
                 ballast_angle = 80
             elif float(self.airmar_data["roll"]) > 20:
@@ -167,30 +165,18 @@ class ControlSystem(Node):  # Gathers data from some nodes and distributes it to
         ballast_json = {"channel": "12", "angle": ballast_angle}
         self.pwm_control_publisher_.publish(self.make_json_string(ballast_json))
 
-    #def headingMovingAverage(self, ):
 
-           
 def main(args=None):
     rclpy.init(args=args)
 
     control_system = ControlSystem()
-    
+
     while rclpy.ok():
-        
-        rclpy.spin_once(control_system, timeout_sec=.5)
-        # Now we have new vals from subscribers in:
-        # control_system.serial_rc
-        # control_system.airmar_data
-        # control_system.trim_tab_status
 
-        # Need to publish new values to both control topics based on new values
-        # control_system.pwm_control_publisher_.publish()       <----- i think both of these are notes from last year and have since been implemented
-        # control_system.trim_tab_control_publisher_.publish()  <----- i think both of these are notes from last year and have since been implemented
+        rclpy.spin_once(control_system, timeout_sec=2)
 
-        #TODO ^^implement
-        
         if len(control_system.serial_rc) < 2:
-            pass # Don't have rc values
+            pass  # Don't have rc values
         elif float(control_system.serial_rc["state2"]) > 600:  # in RC
             control_system.get_logger().info("Currently in RC")
 
@@ -204,7 +190,6 @@ def main(args=None):
                 control_system.trim_tab_control_publisher_.publish(state_msg)
                 control_system.trim_tab_angle_publisher_.publish(angle_msg)
             elif "wind-angle-relative" in control_system.airmar_data:
-                # print(control_system.airmar_data["wind-angle-relative"])
                 try:
                     control_system.find_trim_tab_state(control_system.airmar_data["apparentWind"]["direction"])
                 except Exception as e:
@@ -217,7 +202,7 @@ def main(args=None):
                     ballast_angle = 130
                 elif control_system.serial_rc["ballast"] < 800:
                     ballast_angle = 60
-                ballast_json = {"channel" : "12", "angle" : ballast_angle}
+                ballast_json = {"channel": "12", "angle": ballast_angle}
                 control_system.pwm_control_publisher_.publish(control_system.make_json_string(ballast_json))
             else:
                 control_system.ballast_algorithm()
@@ -227,6 +212,15 @@ def main(args=None):
 
         elif float(control_system.serial_rc["state2"]) < 600:
             control_system.get_logger().error("Currently in AUTONOMOUS")
+
+            # Control Trim Tab
+            if "wind-angle-relative" in control_system.airmar_data:
+                try:
+                    control_system.find_trim_tab_state(control_system.airmar_data["apparentWind"]["direction"])
+                except Exception as e:
+                    control_system.get_logger().error(str(e))
+            else:
+                print("No wind angle values")
 
             # Attempting to read airmar data
             control_system.get_logger().error("Current Heading: " + control_system.airmar_data["currentHeading"])
@@ -276,8 +270,6 @@ def main(args=None):
             #     remaining_time = 10 - (time.time() - current_time)
             #     if remaining_time > 0:
             #         time.sleep(remaining_time)
-
-
 
             # MIN AND MAX FOR RUDDER IS 106.495 AND 32.92
 
