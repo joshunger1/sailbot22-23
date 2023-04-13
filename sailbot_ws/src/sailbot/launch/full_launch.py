6f9081f2fdfea26fe884afbd88bddc2c21b8a901
Joshua Unger <jhunger@wpi.edu>
2023-04-13 16:36:59 -0400

    add fans

--- sailbot_ws/src/sailbot/launch/full_launch.py
+++ sailbot_ws/src/sailbot/launch/full_launch.py
@@ -1,40 +1,56 @@
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-
-# Description:
-# A launch file that simply launches all nodes
 
 
 def generate_launch_description():
-	return LaunchDescription([
-		Node(
-			package='sailbot',
-			node_executable='pwm_controller',
-			name='pwm'
-		),
-		Node(
-			package='sailbot',
-			node_executable='control_system',
-			name='ctrl_sys'
-		),
-		Node(
-			package='sailbot',
-			node_executable='trim_tab_comms',
-			name='trim_tab'
-		),
-		Node(
-			package='sailbot',
-			node_executable='debug_interface',
-			name='debug'
-		),
-		Node(
-			package='sailbot',
-			node_executable='airmar_reader',
-			name='airmar'
-		),
-		Node(
-			package='sailbot',
-			node_executable='serial_rc_receiver',
-			name='rc_rcevr'
-		)
-	])
+    jetson_clocks_fan = ExecuteProcess(
+        cmd=['/usr/bin/env', 'expect', '-f', '-'],
+        input='spawn sudo jetson_clocks --fan\nexpect "Password:"\nsend "YourPasswordHere\\r"\ninteract'
+    )
+
+    pwm = Node(
+        package='sailbot',
+        node_executable='pwm_controller',
+        name='pwm'
+    )
+
+    control_system = Node(
+        package='sailbot',
+        node_executable='control_system',
+        name='ctrl_sys'
+    )
+
+    trim_tab_comms = Node(
+        package='sailbot',
+        node_executable='trim_tab_comms',
+        name='trim_tab'
+    )
+
+    debug_interface = Node(
+        package='sailbot',
+        node_executable='debug_interface',
+        name='debug'
+    )
+
+    airmar_reader = Node(
+        package='sailbot',
+        node_executable='airmar_reader',
+        name='airmar'
+    )
+
+    serial_rc_receiver = Node(
+        package='sailbot',
+        node_executable='serial_rc_receiver',
+        name='rc_rcevr'
+    )
+
+    return LaunchDescription([
+        jetson_clocks_fan,
+        pwm,
+        control_system,
+        trim_tab_comms,
+        debug_interface,
+        airmar_reader,
+        serial_rc_receiver
+    ])
