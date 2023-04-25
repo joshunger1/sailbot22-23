26aa157fcaf4d150fd1827481ab0565bc3fb11e8
Joshua Unger <jhunger@wpi.edu>
2023-04-25 02:35:34 -0400

    heading

--- sailbot_ws/src/sailbot/sailbot/ballast_adc.py
+++ sailbot_ws/src/sailbot/sailbot/ballast_adc.py
@@ -11,7 +11,8 @@ class BallastADC(Node):
         super(BallastADC, self).__init__('ballast_adc')
 
         # info for the Ballast i2c ADC value
-        self.bus = smbus.SMBus(0)  # use 0 for older versions of Jetson Nano
+        self.bus0 = smbus.SMBus(0)  # use 0 for older versions of Jetson Nano
+        self.bus1 = smbus.SMBus(1)  # use 0 for older versions of Jetson Nano
         self.address = 0x48  # address of the adc
         self.ballast_adc_publisher = self.create_publisher(Float32, 'ballast_adc_vals', 10)
 
@@ -34,8 +35,6 @@ class BallastADC(Node):
         self.Device_Address = 0x1e  # HMC5883L magnetometer device address
 
     def calcHeading(self):
-        self.Magnetometer_Init()  # initialize HMC5883L magnetometer
-
         # Read Accelerometer raw value
         x = self.read_raw_data(self.X_axis_H)
         z = self.read_raw_data(self.Z_axis_H)
@@ -59,8 +58,8 @@ class BallastADC(Node):
 
     def read_raw_data(self, addr):
         # Read raw 16-bit value
-        high = self.bus.read_byte_data(self.Device_Address, addr)
-        low = self.bus.read_byte_data(self.Device_Address, addr + 1)
+        high = self.bus1.read_byte_data(self.Device_Address, addr)
+        low = self.bus1.read_byte_data(self.Device_Address, addr + 1)
 
         # concatenate higher and lower value
         value = ((high << 8) | low)
@@ -72,13 +71,13 @@ class BallastADC(Node):
 
     def Magnetometer_Init(self):
         # write to Configuration Register A
-        self.bus.write_byte_data(self.Device_Address, self.Register_A, 0x70)
+        self.bus1.write_byte_data(self.Device_Address, self.Register_A, 0x70)
 
         # Write to Configuration Register B for gain
-        self.bus.write_byte_data(self.Device_Address, self.Register_B, 0xa0)
+        self.bus1.write_byte_data(self.Device_Address, self.Register_B, 0xa0)
 
         # Write to mode Register for selecting mode
-        self.bus.write_byte_data(self.Device_Address, self.Register_mode, 0)
+        self.bus1.write_byte_data(self.Device_Address, self.Register_mode, 0)
 
     def timer_callback(self):
         self.calcADC()
@@ -86,18 +85,18 @@ class BallastADC(Node):
 
     def calcADC(self):
         config = [0x40, 0x83]
-        self.bus.write_i2c_block_data(self.address, 0x01, config)
+        self.bus0.write_i2c_block_data(self.address, 0x01, config)
         time.sleep(0.1)
         # read the data from the ADC
-        data = self.bus.read_i2c_block_data(self.address, 0x00)
+        data = self.bus0.read_i2c_block_data(self.address, 0x00)
         reading1 = (((data[0] << 8) & 0xFF00) + (data[1] & 0x00FF))  # Analog pin from POT
 
         time.sleep(0.1)
         # convert the data to a voltage
         config = [0x50, 0x83]  # configuration for the ADC
-        self.bus.write_i2c_block_data(self.address, 0x01, config)  # tell the ADC to read off of the other pin
+        self.bus0.write_i2c_block_data(self.address, 0x01, config)  # tell the ADC to read off of the other pin
         time.sleep(0.1)
-        data = self.bus.read_i2c_block_data(self.address, 0x00)
+        data = self.bus0.read_i2c_block_data(self.address, 0x00)
         reading2 = (((data[0] << 8) & 0xFF00) + (data[1] & 0x00FF))  # supply voltage
 
         feedback_voltage = reading1 * 6.144 / 32767.0
@@ -112,6 +111,7 @@ def main(args=None):
     rclpy.init(args=args)
 
     ballast_adc = BallastADC()
+    ballast_adc.Magnetometer_Init()
 
     rclpy.spin(ballast_adc)
 
