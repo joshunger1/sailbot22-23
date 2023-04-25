347b217a7431d2ebe843267622788c83cb7c0595
sailbot <avthammana@wpi.edu>
2023-04-25 01:51:47 -0400

    heading vals

--- sailbot_ws/src/sailbot/sailbot/ballast_adc.py
+++ sailbot_ws/src/sailbot/sailbot/ballast_adc.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import Int8, Int16, Float32
 import time
 import smbus
+import math
 
 class BallastADC(Node):
     def __init__(self):
@@ -11,10 +12,77 @@ class BallastADC(Node):
         # info for the Ballast i2c ADC value
         self.bus = smbus.SMBus(0)  # use 0 for older versions of Jetson Nano
         self.address = 0x48  # address of the adc
-        self.ballast_adc_publisher = self.create_publisher(Float32, 'ballast_adc_vals', 10)  # Wind direction
+        self.ballast_adc_publisher = self.create_publisher(Float32, 'ballast_adc_vals', 10)
+
+        # info for the Heading i2c value
+        self.heading_adc_publisher = self.create_publisher(Float32, 'heading_adc_publisher', 10)
 
         timer_period = 0.5  # Fetch data every 1 second
         self.timer = self.create_timer(timer_period, self.timer_callback)
+
+	#some MPU6050 Registers and their Address
+        self.Register_A     = 0              #Address of Configuration register A
+        self.Register_B     = 0x01           #Address of configuration register B
+        self.Register_mode  = 0x02           #Address of mode register
+
+        self.X_axis_H    = 0x03              #Address of X-axis MSB data register
+        self.Z_axis_H    = 0x05              #Address of Z-axis MSB data register
+        self.Y_axis_H    = 0x07              #Address of Y-axis MSB data register
+        self.declination = -0.00669          #define declination angle of location where measurement going to be done
+        self.pi          = 3.14159265359     #define pi value
+
+
+    def calcHeading(self):
+        bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+        Device_Address = 0x1e   # HMC5883L magnetometer device address
+
+        Magnetometer_Init()     # initialize HMC5883L magnetometer 
+	
+        #Read Accelerometer raw value
+        x = read_raw_data(self.X_axis_H)
+        z = read_raw_data(self.Z_axis_H)
+        y = read_raw_data(self.Y_axis_H)
+
+        heading = math.atan2(y, x) + self.declination
+        
+        #Due to declination check for >360 degree
+        if(heading > 2*self.pi):
+                heading = heading - 2*self.pi
+
+        #check for sign
+        if(heading < 0):
+                heading = heading + 2*self.pi
+
+        #convert into angle
+        heading_angle = int(heading * 180/self.pi)
+        my_float_msg = Float32()
+        my_float_msg.data = heading_angle
+        self.ballast_adc_publisher.publish(my_float_msg)
+
+
+    def read_raw_data(self, addr):
+        #Read raw 16-bit value
+        high = bus.read_byte_data(Device_Address, addr)
+        low = bus.read_byte_data(Device_Address, addr+1)
+
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
+
+        #to get signed value from module
+        if(value > 32768):
+            value = value - 65536
+        return value
+
+
+    def Magnetometer_Init(self):
+        #write to Configuration Register A
+        self.bus.write_byte_data(Device_Address, self.Register_A, 0x70)
+
+        #Write to Configuration Register B for gain
+        self.bus.write_byte_data(Device_Address, self.Register_B, 0xa0)
+
+        #Write to mode Register for selecting mode
+        self.bus.write_byte_data(Device_Address, self.Register_mode, 0)
 
     def timer_callback(self):
         self.calcADC()
