9073a335ce93f80370a345d588d64ef48e9d3d8f
Joshua Unger <jhunger@wpi.edu>
2023-04-26 00:31:30 -0400

    cv framerate

--- sailbot_ws/src/sailbot/sailbot/test.py
+++ sailbot_ws/src/sailbot/sailbot/test.py
@@ -1,5 +1,5 @@
 import sys
-
+import time
 sys.path.append('c:\python37\lib\site-packages')
 
 import cv2
@@ -10,6 +10,8 @@ orange_upper = (20, 255, 255)
 
 # Set the minimum area requirement for an object to be considered
 min_area = 7500
+frame_count = 0
+start_time = time.time()
 
 # Open the default camera
 cap = cv2.VideoCapture("/dev/video0")
@@ -42,6 +44,15 @@ while True:
 
     # Display the camera view and the neon orange blob detection with a bounding box
     cv2.imshow('Camera View and Neon Orange Blob Detection', frame)
+
+    # Add frame rate calculation and display
+    frame_count += 1
+    elapsed_time = time.time() - start_time
+    if elapsed_time > 1:
+        fps = frame_count / elapsed_time
+        cv2.putText(frame, f'FPS: {int(fps)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        frame_count = 0
+        start_time = time.time()
 
     # Wait for a key press and check if the user pressed the 'q' key
     if cv2.waitKey(1) & 0xFF == ord('q'):
